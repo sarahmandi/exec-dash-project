0fa7e16196e39250883b0188fce758603a048a26
Sarah Mandi <snm62@georgetown.edu>
2019-02-10 19:29:44 -0500

    Add data visualization

--- monthly_sales.py
+++ monthly_sales.py
@@ -5,7 +5,9 @@ import csv
 import os
 import itertools
 from operator import itemgetter
-
+import matplotlib.pyplot as plt; plt.rcdefaults()
+import numpy as np
+import matplotlib.pyplot as plt
 
 ##Option C: display list of files to user
 path = "/Users/sarahmandi1/Desktop/exec-dash-project/data"
@@ -46,8 +48,8 @@ for product, product_rows in rows_by_product:
     product_sales.append({"name": product, "monthly_sales": monthly_sales})
 
 sorted_product_sales = sorted(product_sales, key=itemgetter("monthly_sales"), reverse=True)
-top_sellers = sorted_product_sales[0:7] # first three items in a list :-)
- 
+top_sellers = sorted_product_sales[0:7] 
+#print(top_sellers)
 
 month = "MARCH" # TODO: get from file name or date values
 year = 2018 # TODO: get from file name or date values
@@ -68,15 +70,34 @@ print(f"TOTAL SALES: {to_usd(total_sales)}")
 print("-------------------------")
 print("TOP SELLING PRODUCTS:")
 
+top_seller_names = []
+top_seller_sales = []
+
 counter = 0
 for top_seller in top_sellers:
     counter = counter + 1
     product_name = top_seller["name"]
     sales_usd = to_usd(top_seller["monthly_sales"])
+    sales = top_seller["monthly_sales"]
     print(f"  {counter}. {product_name} ({sales_usd})")
+    top_seller_names.append(product_name)
+    top_seller_sales.append(sales)
+
+#print(top_seller_names)
+#print(top_seller_sales)
 ####
 
 print("-----------------------")
 print("VISUALIZING THE DATA...")
 
 
+objects = top_seller_names
+y_pos = np.arange(len(objects))
+performance = top_seller_sales
+ 
+plt.barh(y_pos, performance, align='center', alpha=0.5)
+plt.yticks(y_pos, objects)
+plt.xlabel('Sales')
+plt.title('Top-Selling Products (' + month + " " + str(year) +")")
+ 
+plt.show()
